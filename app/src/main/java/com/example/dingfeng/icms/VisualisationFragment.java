95b7e3af6d8cc441f876013476dccbe4eeacd877
dingfs <dingfengshao@gmail.com>
2016-04-06 21:51:31 -0400

    Setup UI and get data from resultActivity

--- app/src/main/java/com/example/dingfeng/icms/VisualisationFragment.java
+++ app/src/main/java/com/example/dingfeng/icms/VisualisationFragment.java
@@ -1,13 +1,23 @@
 package com.example.dingfeng.icms;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.net.URI;
 
 public class VisualisationFragment extends android.support.v4.app.Fragment {
 
     public static final String ARG_OBJECT = "object";
+    private ImageView image_view;
+    private Uri uri;
+    private String result;
+    private TextView text_view;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater,
@@ -20,6 +30,15 @@ public class VisualisationFragment extends android.support.v4.app.Fragment {
     public void onActivityCreated(Bundle savedInstanceState) {
 
         super.onActivityCreated(savedInstanceState);
+
+        image_view = (ImageView)getActivity().findViewById(R.id.image_view);
+        text_view=(TextView)getActivity().findViewById(R.id.text_view);
+
+        uri = ((ResultActivity)getActivity()).getImageURI();
+        image_view.setImageURI(uri);
+
+        result=((ResultActivity)getActivity()).getRecognizedText();
+        text_view.setText(result);
 
 
     }
